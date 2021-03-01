46279d25883e532e8fdb1952f3aed44f482fad65
Tachun Lin <djlin@bradley.edu>
2021-03-01 16:15:06 -0600

    solutions for C/Python/Go

---
+++ assignments/assignment1/client_server/server-go-sol.go
@@ -0,0 +1,69 @@
+/*****************************************************************************
+ * server-go.go                                                                 
+ * Name: Tachun Lin
+ *****************************************************************************/
+
+package main
+
+import (
+  "fmt"
+//  "io"
+  "os"
+  "log"
+  "net"
+  "bufio"
+)
+
+const RECV_BUFFER_SIZE = 2048
+
+/* TODO: server()
+ * Open socket and wait for client to connect
+ * Print received message to stdout
+*/
+func server(server_port string) {
+  ln, err := net.Listen("tcp", ":"+server_port)
+  if err != nil {
+    // handle error
+    fmt.Println("Error listening:", err.Error())
+    os.Exit(1)
+  }
+  defer ln.Close()
+  
+  for {
+    conn, err := ln.Accept()
+    if err != nil {
+      // handle error
+      fmt.Println("Error accepting:", err.Error())
+      return
+    }
+
+    defer conn.Close()
+
+    var (
+      buf = make([]byte, RECV_BUFFER_SIZE)
+      r   = bufio.NewReader(conn)
+    )
+
+    for {
+      n, err := r.Read(buf)
+      if n < 0 {break}
+      if err != nil {
+	//panic(err)
+    	conn.Close()
+    	break
+      }
+      data := string(buf[:n])
+      fmt.Print(data)
+    }
+  }
+}
+
+
+// Main parses command-line arguments and calls server function
+func main() {
+  if len(os.Args) != 2 {
+    log.Fatal("Usage: ./server-go [server port]")
+  }
+  server_port := os.Args[1]
+  server(server_port)
+}
---
+++ assignments/assignment1/client_server/server-python-sol.py
@@ -0,0 +1,45 @@
+###############################################################################
+# server-python.py
+# Name: Tachun Lin
+###############################################################################
+
+import sys
+import socket
+
+RECV_BUFFER_SIZE = 2048
+QUEUE_LENGTH = 10
+
+
+
+def server(server_port):
+    """TODO: Listen on socket and print received message to sys.stdout"""
+    # create an INET, STREAMing socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
+        # bind the socket to the host and its port
+        serversocket.bind(('', server_port))
+        # prepare for connection
+        serversocket.listen(QUEUE_LENGTH)
+
+        while True:
+            # accept connections from outside
+            (clientsocket, address) = serversocket.accept()
+
+            with clientsocket:
+                while True:
+                    # receive data and print it out
+                    data = clientsocket.recv(RECV_BUFFER_SIZE)
+                    if not data: break
+                    sys.stdout.buffer.raw.write(data)
+                sys.stdout.flush()
+    pass
+
+
+def main():
+    """Parse command-line argument and call server function """
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python server-python.py [Server Port]")
+    server_port = int(sys.argv[1])
+    server(server_port)
+
+if __name__ == "__main__":
+    main()
